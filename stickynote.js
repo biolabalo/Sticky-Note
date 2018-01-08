d0bc96dff1837930d24d1a4c9210f6660193ace1
balogun <balogunbiola101@gmail.com>
2018-01-08 15:14:01 +0100

    sticky note with jquery

---
+++ stickynote.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function(){
+   
+    
+    $("#btn2").click(function(){
+
+     $("#box").clone().appendTo("#container");
+    });
+    
+    
+  
+
+    
+    
+    
+    
+    $("#btn1").click(function(){
+        $("#box").remove();
+    }); 
+    
+    
+    
+    
+      
+      });
+    
+    
+    
+    
+    
+
+
+
+
+
+
+    
